8fcc3eea7dac3563f8d830e3f667a15338caefdd
ks2842 <51949666+ks2842@users.noreply.github.com>
2019-06-30 11:31:19 +0530

    Create 56.py

---
+++ 56.py
@@ -0,0 +1,13 @@
+k=str(input())
+p=0
+l=0
+m=len(k)
+for i in range(0,m):
+    if(k[i].isalpha()):
+       p=p+1
+    if(k[i].isnumeric()):
+       l=l+1
+if((p==0) or (l==0)):
+    print("no")
+else:
+    print("yes")
